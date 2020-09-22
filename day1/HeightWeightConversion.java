682c932ebb3440b9b5a741c8bd4cfcb12dc9e8ea
varsha-dev1 <71543472+varsha-dev1@users.noreply.github.com>
2020-09-22 15:04:06 +0530

    Update HeightWeightConversion.java

--- day1/HeightWeightConversion.java
+++ day1/HeightWeightConversion.java
@@ -1,25 +1,19 @@
+// Batch2 Day2 Problem2
+
 import java.util.Scanner;
-
-public class HeightWeightConversion {
-
-	public static void main(String[] args) {
-		
-		
+public class HeightWeightConversion 
+{
+	public static void main(String[] args) 
+	{		
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Feet: ");
 		int feet = scan.nextInt();
 		System.out.print("Inch: ");
 		int inch = scan.nextInt();
 		System.out.print("Kg: ");
-		int kg = scan.nextInt();
-		
-		int heightInInch = (feet*12)+inch;
-
-		
+		int kg = scan.nextInt();		
+		int heightInInch = (feet*12)+inch;		
 		System.out.print("Height in cm: " + heightInInch * 2.54f);
 		System.out.print("\nWeight in pounds: " + kg * 2.2f);
-		
-
 	}
-
 }
